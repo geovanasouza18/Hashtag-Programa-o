cc2444376627c6546a0dd1d13471aaf08812fa6a
Geovana Souza <geovanasouza0712@gmail.com>
2025-12-16 22:10:05 -0300

    mini curso

--- relatorio_vendas.py
+++ relatorio_vendas.py
@@ -8,10 +8,14 @@ pd.set_option('display.max_columns', None) #mostrar máximo de coluna
 print(tabelas_vendas)
 
 #faturamento de cada loja
-
+faturamento = tabelas_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum() # filtrou as tabelas e somou o valor total
+print(faturamento)
 
 #quantidade de produtos vendidos de cada loja
+quantidade = tabelas_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
+print(quantidade)
 
 #ticket médio por produto de cada loja
+
 
 #enviar relatório para um email
