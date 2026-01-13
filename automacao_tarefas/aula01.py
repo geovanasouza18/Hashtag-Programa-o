2deeb719a1229ccc8538502d3f7a9018763df7e5
Geovana Souza <geovanasouza0712@gmail.com>
2026-01-12 21:17:36 -0300

    em andamento

--- automacao_tarefas/aula01.py
+++ automacao_tarefas/aula01.py
@@ -22,8 +22,33 @@ pyautogui.press("tab")
 pyautogui.write('python123')
 pyautogui.click(x=954, y=566)
 time.sleep(3)
-pyautogui.hotkey('alt', 'tab')
 
 #Lendo um arquivo com o pandas
 tabela = pd.read_csv(r'C:\Users\User\PycharmProjects\MiniCursoPython - Hashtang\automacao_tarefas\produtos.csv')
 print(tabela)
+for linha in tabela.index:
+    #Cadastrar um produto
+    pyautogui.click(x=743, y=285)
+    #Codigo
+    pyautogui.write('MOLO000251')
+    pyautogui.press('tab')
+    #Marca
+    pyautogui.write('Logitech')
+    pyautogui.press('tab')
+    #Tipo
+    pyautogui.write('Mouse')
+    pyautogui.press('tab')
+    #categoria
+    pyautogui.write('1')
+    pyautogui.press('tab')
+    #Preço unitário
+    pyautogui.write('25.95')
+    pyautogui.press('tab')
+    #Custo
+    pyautogui.write('6.50')
+    pyautogui.press('tab')
+    #OBS
+    pyautogui.write('obs')
+    pyautogui.press('tab')
+
+    pyautogui.press('enter')
