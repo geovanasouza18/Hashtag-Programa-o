a05bc967ed629dd014fe9ff2061446a164bd76c5
Geovana Souza <geovanasouza0712@gmail.com>
2026-01-20 21:21:50 -0300

    atualizações

--- python_impressionador/fundamentos/07-strings.py
+++ python_impressionador/fundamentos/07-strings.py
@@ -1,2 +1,10 @@
 texto = 'Geovana é linda'
 print(len(texto))
+
+faturamento = 20000
+custo = 15400
+lucro = faturamento - custo
+print(f'O faturamento foi de {faturamento}, o custo foi de {custo} e o lucro foi de {lucro}')
+
+print('@' in 'geovana@gmail.com')
+print('@' in 'geovanagmail.com')
