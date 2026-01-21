15c19ab9590f6e11df2d050672e55a297199a06b
Geovana Souza <geovanasouza0712@gmail.com>
2026-01-20 21:35:05 -0300

    atualizações

--- python_impressionador/fundamentos/07-strings.py
+++ python_impressionador/fundamentos/07-strings.py
@@ -8,3 +8,16 @@ print(f'O faturamento foi de {faturamento}, o custo foi de {custo} e o lucro foi
 
 print('@' in 'geovana@gmail.com')
 print('@' in 'geovanagmail.com')
+
+texto1 = 'Programação'
+print(texto1.casefold())
+print(texto1.lower())
+
+#endswith - usado para verificar se o texto termina com um valor específico e dar a resposta True or False
+print(texto.endswith('linda'))
+
+frase = '''Progamação em Python
+Aprendendo a trabalhar com Strings
+Python Impressionador
+'''
+print(frase.splitlines())
